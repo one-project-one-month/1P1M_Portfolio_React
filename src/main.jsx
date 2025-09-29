e6b07a38d5c4edd3f5922081d833f598e9fe27e0
SayarGyi007-lab <thss152021@gmail.com>
2025-09-29 09:50:04 +0000

    initial utility

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store";
+// import React from "react";
+//  import ReactDOM from "react-dom/client";
+// import { Provider } from "react-redux";
+// import { store } from "./store";
+ import { StrictMode } from "react";
 import App from "./App";
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
+
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </Provider>
+  </StrictMode>
 );
+
+
