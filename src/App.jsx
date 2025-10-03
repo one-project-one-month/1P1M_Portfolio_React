16a2980c51df589f802df1e7f396d4261008e358
Lwin Min Thein <lwinthein679@gmail.com>
2025-10-03 15:57:51 +0630

    add DevForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import TechStack from './app/constants/TechStack'
 import Button from './app/components/ui/Button'
 import Background from './app/components/ui/Background'
 import FormBackground from './app/components/ui/FormBackground'
 import FormField from './app/components/ui/FormFields'
 import RegisterPage from './app/features/auth/pages/RegisterPage'
 import LoginForm from './app/features/auth/components/LoginForm'
+import DevProfilePage from './app/features/auth/pages/DevProfilePage'
+import FormDropdown from './app/components/ui/FormDropdown'
 
 
 function App() {
 
   return <>
+
+
+
   <Background className='h-[100vh]'>
+
+   <FormDropdown menuList={TechStack}/>
+
+
   <div>hello</div>
   <div className='p-10 space-x-4 space-y-4'>
   <Button variant="primary" size="primary">Continue</Button>
@@ -46,6 +56,18 @@ function App() {
 <RegisterPage />
 
       <LoginForm></LoginForm>
+
+
+
+
+
+      <DevProfilePage/>
+
+
+
+
+
+     
   </>
 }
 
