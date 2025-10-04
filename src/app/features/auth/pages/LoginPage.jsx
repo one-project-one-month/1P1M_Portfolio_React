80d90f10bfe464d41c1a326d04aa2431529dfd75
Kaunghtet210310 <williamnaing210310@gmail.com>
2025-10-05 02:48:58 +0630

    integrate LoginPage Route

---
+++ src/app/features/auth/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import LoginForm from "../components/LoginForm";
+import Background from "@/components/ui/Background";
+
+function LoginPage () {
+    return(
+    <>
+        <Background className="h-screen flex items-center justify-center">
+            <div className="w-[468px] h-[448px] border border-[#1E2939] rounded-3xl">
+                <LoginForm></LoginForm>
+            </div>
+        </Background>
+    </>
+
+    );
+}
+
+export default LoginPage;
