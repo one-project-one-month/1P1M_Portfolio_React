361a9cc9ca6fd3bf5c40f0141689e3a12e40f0bd
Lwin Min Thein <lwinthein679@gmail.com>
2025-11-09 21:57:31 +0630

    fix issues for card

--- src/app/features/auth/pages/ProjectPortfolioList.jsx
+++ src/app/features/auth/pages/ProjectPortfolioList.jsx
@@ -146,8 +146,7 @@ function ProjectPortfolioList() {
                         sm:grid-cols-1 
                         md:grid-cols-2 
                         lg:grid-cols-3 
-                        xl:grid-cols-3 
-                        2xl:grid-cols-4"
+                       "
         >
           {projects.length > 0 ? (
             projects.map((project) => (
