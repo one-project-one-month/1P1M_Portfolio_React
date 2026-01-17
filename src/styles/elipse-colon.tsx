6b7d59388cfc8155cf60cf788f422463eb0c1830
Lwin Min Thein <lwinthein679@gmail.com>
2026-01-17 10:39:45 +0630

    fix countdown

--- src/styles/elipse-colon.tsx
+++ src/styles/elipse-colon.tsx
@@ -1,7 +1,6 @@
 export const EllipseColon = () => (
-    <div className="h-[151.7559px]  flex flex-col  mt-16 gap-[10px]">
-      <span className="w-[10px] h-[9.7207px] rounded-full bg-white" />
-      <span className="w-[10px] h-[9.7207px] rounded-full bg-white" />
-    </div>
-  )
-  
+  <div className="h-[151.7559px]  flex flex-col  mt-16 gap-[10px]">
+    <span className="w-[10px] h-[9.7207px] rounded-full bg-white" />
+    <span className="w-[10px] h-[9.7207px] rounded-full bg-white" />
+  </div>
+);
