e730dbea62b823f602e1df28d6e39278e0255c8c
Jeff307-kib <wunnatunsai940@gmail.com>
2026-01-29 20:44:30 +0630

    feat(about-us) fix build fail, add types

--- src/app/features/about-us/components/social-link-icon.tsx
+++ src/app/features/about-us/components/social-link-icon.tsx
@@ -1,4 +1,11 @@
-const SocialLink = ({ Icon, href }) => (
+import type { LucideIcon } from 'lucide-react';
+
+interface SocialLinkProps {
+  Icon: LucideIcon;
+  href: string;
+}
+
+const SocialLink = ({ Icon, href }: SocialLinkProps) => (
   <a
     href={href}
     className="p-2 border-none rounded-lg hover:bg-[#FFBA00] hover:text-black transition-all duration-300 text-[#FFBA00]"
