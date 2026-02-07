fce752d4df012a6a6806f3e2a114f3b13cdc98ce
winminthantdev <winminthant.gwin@gmail.com>
2026-02-07 19:37:02 +0630

    fix : deploy failed

--- src/app/features/admin-profile/hooks/user-profile.ts
+++ src/app/features/admin-profile/hooks/user-profile.ts
@@ -1,5 +1,6 @@
 import { profileService } from '@/app/features/admin-profile/services/profile-service.ts';
 import { useToast } from '@/components/ui/toast-provider.tsx';
+import { useUserInfoStore } from '@/store/user-info-store.ts';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
@@ -9,7 +10,10 @@ import { type ProfileFormValues, ProfileSchema } from '../services/types.ts';
 export const useProfile = (userId: number | null) => {
   const [isEditing, setIsEditing] = useState(false);
   const queryClient = useQueryClient();
-  const addToast = useToast();
+  const { addToast } = useToast();
+
+  const user = useUserInfoStore((state) => state.userInfo);
+  const userID = user?.userId ? parseInt(String(user.userId)) : null;
 
   const { data, isLoading: isFetching } = useQuery({
     queryKey: ['profile', userId],
@@ -29,15 +33,19 @@ export const useProfile = (userId: number | null) => {
   });
 
   const mutation = useMutation({
-    mutationFn: (payload: ProfileFormValues) =>
-      profileService.updateProfile(payload),
+    mutationFn: (payload: ProfileFormValues) => {
+      if (!userID) throw new Error('User ID is missing');
+      return profileService.updateProfile(userID, payload);
+    },
     onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: ['profile', userId] });
+      await queryClient.invalidateQueries({ queryKey: ['profile', userID] });
       setIsEditing(false);
       addToast('Profile Updated Successfully!', 'success');
     },
     onError: (error) => {
       console.error('Update failed:', error);
+      // Optional: Add an error toast here
+      addToast('Failed to update profile.', 'error');
     },
   });
 
