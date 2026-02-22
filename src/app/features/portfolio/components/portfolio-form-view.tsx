97817c08bbee371056a9ce0e0746e9d0d641a6ce
thureinhtet99 <ignite25399@gmail.com>
2026-02-22 17:08:19 +0700

    refactor: enhance layout and styling of portfolio components

--- src/app/features/portfolio/components/portfolio-form-view.tsx
+++ src/app/features/portfolio/components/portfolio-form-view.tsx
@@ -1,12 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import type { ProjectData } from '../../portfolio-management/constants/data';
 import ProjectPortfolioForm from './project-portfolio-form';
 
 const PortfolioFormview = () => {
   const navigate = useNavigate();
 
-  const handleSave = (data: Partial<ProjectData>) => {
-    console.log('Creating project:', data);
+  const handleSave = () => {
     navigate('/portfolios');
   };
 
@@ -15,13 +13,16 @@ const PortfolioFormview = () => {
   };
 
   return (
-    <div className="flex justify-center items-center">
-      <ProjectPortfolioForm
-        mode="create"
-        onSave={handleSave}
-        onCancel={handleCancel}
-        onClose={handleCancel}
-      />
+    <div className="relative left-1/2 w-screen -translate-x-1/2">
+      <div className="absolute inset-0 bg-[#080D22]/50 backdrop-blur-xs" />
+      <div className="relative z-10 flex min-h-[calc(100dvh-7rem)] w-full items-center justify-center overflow-y-auto p-4">
+        <ProjectPortfolioForm
+          mode="create"
+          onSave={handleSave}
+          onCancel={handleCancel}
+          onClose={handleCancel}
+        />
+      </div>
     </div>
   );
 };
