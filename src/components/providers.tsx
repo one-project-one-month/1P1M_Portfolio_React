74ad086a2502cbdb01898456ed0173fa660ab573
Lwin Min Thein <lwinminthein.mhs@gmail.com>
2026-04-30 10:57:47 +0630

    fix:build error

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import type { ReactNode } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { ToastProvider } from './ui/toast-provider';
